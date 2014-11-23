7fc3fc0649b66794477116279b9ca30680b26352
Thomas Noe <thomasjnoe@gmail.com>
2014-11-22 20:17:30 -0500

    Add remember digest and token attributes

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -5,12 +5,18 @@ module SessionsHelper
 		session[:user_id] = user.id 
 	end
 
-	# Returns the current logged-in user (if any)
+	# Logs out the current user.
+	def log_out
+		session.delete(:user_id)
+		@current_user = nil
+	end
+
+	# Returns the current logged-in user (if any).
 	def current_user
 		@current_user ||= User.find_by(id: session[:user_id])
 	end
 
-	# Returns true if the user is logged in, false otherwise
+	# Returns true if the user is logged in, false otherwise.
 	def logged_in?
 		!current_user.nil?
 	end
