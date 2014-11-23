bf8d9408ec6c6f619dd2ae8a5c2bc7d53c0929b9
Thomas Noe <thomasjnoe@gmail.com>
2014-11-23 12:04:08 -0500

    Add friendly forwarding on login

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -35,6 +35,17 @@ module SessionsHelper
 		!current_user.nil?
 	end
 
+	# Redirects to stored location (or to the default).
+	def redirect_back_or(default)
+		redirect_to(session[:forwarding_url] || default)
+		session.delete(:forwarding_url)
+	end
+
+	# Stores the URL trying to be accessed.
+	def store_location
+		session[:forwarding_url] = request.url if request.get?
+	end
+
 	# Forgets a persistent session.
 	def forget(user)
 		user.forget
