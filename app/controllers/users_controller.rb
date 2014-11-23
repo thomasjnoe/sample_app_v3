e5998ac3fef81871491b56b1de30350cfe8ab918
Thomas Noe <thomasjnoe@gmail.com>
2014-11-23 00:28:06 -0500

    Finish requiring correct user for edting/updating

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :logged_in_user, only: [:edit, :update]
+  before_action :correct_user, only: [:edit, :update]
 
   def new
   	@user = User.new
@@ -21,11 +22,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
+    # @user assignment through correct_user
   end
 
   def update
-    @user = User.find(params[:id])
+    # @user assignment through correct_user
     if @user.update_attributes(user_params)
       # Handle a successful update
       flash[:success] = "Profile updated"
@@ -49,5 +50,10 @@ class UsersController < ApplicationController
         flash[:danger] = "Please log in."
         redirect_to login_url
       end
+    end
+
+    def correct_user
+      @user = User.find(params[:id])
+      redirect_to(root_url) unless current_user?(@user)
     end
 end
