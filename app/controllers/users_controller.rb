51917c38f7c71c2935cafb220595702cea713e17
Thomas Noe <thomasjnoe@gmail.com>
2014-11-22 23:40:24 -0500

    Add edit profile functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def new
   	@user = User.new
   end
@@ -16,6 +17,10 @@ class UsersController < ApplicationController
   	else
   		render 'new'
   	end
+  end
+
+  def edit
+    @user = User.find(params[:id])
   end
 
   private
