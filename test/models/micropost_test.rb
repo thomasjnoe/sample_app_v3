5b07518717a5ffe9161e338bded1c75c2e5047b7
Thomas Noe <thomasjnoe@gmail.com>
2014-12-09 23:43:41 -0500

    Add content validation to micropost

--- test/models/micropost_test.rb
+++ test/models/micropost_test.rb
@@ -16,4 +16,14 @@ class MicropostTest < ActiveSupport::TestCase
   	@micropost.user_id = nil
   	assert_not @micropost.valid?
   end
+
+  test "content should be present" do 
+  	@micropost.content = "    "
+  	assert_not @micropost.valid?
+  end
+
+  test "content should be 140 characters or less" do 
+  	@micropost.content = "a" * 141
+  	assert_not @micropost.valid?
+  end
 end
