053546b3c39cd28bee38c8b9ec7ca43b5690ac71
Tommy Noe <tnoe@appirio.com>
2015-01-14 23:39:59 -0500

    Add follow, unfollow and following? methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
 	has_many :active_relationships, class_name: "Relationship",
 									foreign_key: "follower_id",
 									dependent: :destroy
+	has_many :following, through: :active_relationships, source: :followed
 	before_save { self.email = email.downcase }
 	validates :name, presence: true, length: { maximum: 50 }
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
@@ -47,4 +48,22 @@ class User < ActiveRecord::Base
 	def feed
 		Micropost.where("user_id = ?", id)
 	end
+
+	# Follows a user.
+	def follow(other_user)
+		active_relationships.create(followed_id: other_user.id)
+	end
+
+	def unfollow(other_user)
+		active_relationships.find_by(followed_id: other_user.id).destroy
+	end
+
+	def following?(other_user)
+		following.include?(other_user)
+	end
 end
+
+
+
+
+
