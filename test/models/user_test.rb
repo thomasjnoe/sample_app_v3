98101255cd43d5ca46122bf979c4a0f432d4b248
Thomas Noe <thomasjnoe@gmail.com>
2015-01-17 21:40:00 -0500

    Add completed status feed with tests

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -89,6 +89,24 @@ class UserTest < ActiveSupport::TestCase
     michael.unfollow(archer)
     assert_not michael.following?(archer)
   end
+
+  test "feed should have the right posts" do 
+    michael = users(:michael)
+    archer = users(:archer)
+    lana = users(:lana)
+    # Posts from a followed user
+    lana.microposts.each do |post_following|
+      assert michael.feed.include?(post_following)
+    end
+    # Posts from self
+    michael.microposts.each do |post_self|
+      assert michael.feed.include?(post_self)
+    end
+    # Posts from an unfollowed user
+    archer.microposts.each do |post_unfollowed|
+      assert_not michael.feed.include?(post_unfollowed)
+    end
+  end
 end
 
 
